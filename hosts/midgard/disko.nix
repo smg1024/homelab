ab2e8d05fd013b9a0c5c79f1b97621fff9a61e7b
Sangmin Kim <smg981024@kmeat.com>
2026-05-19 14:41:25 +0900

    add disko ext4 templates

--- hosts/midgard/disko.nix
+++ hosts/midgard/disko.nix
@@ -1,5 +1,37 @@
 { ... }:
 
 {
-  # TODO: Define the midgard disk layout.
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/disk/by-id/REPLACE_WITH_MIDGARD_DISK_ID";
+
+      content = {
+        type = "gpt";
+
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+
+          root = {
+            size = "100%";
+
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+        };
+      };
+    };
+  };
 }
