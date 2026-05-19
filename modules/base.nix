ea767d86e0c951ff0b019daa522f2ee4836705a7
Sangmin Kim <smg981024@kmeat.com>
2026-05-19 13:26:45 +0900

    add base system module

--- modules/base.nix
+++ modules/base.nix
@@ -1,1 +1,37 @@
-# TODO: Define common base system settings.
+{ pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nix.settings.trusted-users = [
+    "root"
+    "@wheel"
+  ];
+
+  time.timeZone = "Asia/Seoul";
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+  networking.firewall.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    wget
+    htop
+    tmux
+    jq
+    fd
+    ripgrep
+    lsof
+    pciutils
+    usbutils
+    dnsutils
+  ];
+}
